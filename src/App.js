c8b10ddbc336dfbafa0ec77811f4982201a12b17
PSortman <pearlsortman@gmail.com>
2017-09-18 20:14:57 -0400

    Navbar and replacing default text

--- src/App.js
+++ src/App.js
@@ -7,8 +7,31 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
+          <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <a className="navbar-brand" href="#">Navbar</a>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Pricing</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#">Disabled</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <div>
           <h2>Hoppi Space</h2>
-          <h3>Coming Soon!</h3>
         </div>
       </div>
     );
